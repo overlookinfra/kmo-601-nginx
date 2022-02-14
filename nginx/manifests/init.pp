9eb36486f3925626474f4cd42d5d340becd0122b
awilliamson-puppet <alex.williamson@puppet.com>
2022-02-14 16:49:29 -0500

    lab 2.0 end state and lab 2.1 start state

--- nginx/manifests/init.pp
+++ nginx/manifests/init.pp
@@ -1,16 +1,19 @@
 # @summary A short summary of the purpose of this class
-#
 # A description of what this class does
-#
 # @example
 #   include nginx
 class nginx {
+  $package_provider = $facts['os']['family'] ? {
+    'windows' => 'chocolatey',
+    default   => undef,
+  }
   service { 'nginx':
     ensure  => running,
     enable  => true,
     require => Package['nginx'],
   }
   package { 'nginx':
-    ensure => latest,
+    ensure   => latest,
+    provider => $package_provider,
   }
 }
