05b59e6cb3fcb62372ce480caf0f5d8177944410
Alex Williamson <89106273+awilliamson-puppet@users.noreply.github.com>
2022-06-16 09:26:17 -0400

    Create backup_windows_logs.ps1

---
+++ tasks/backup_windows_logs.ps1
@@ -0,0 +1,20 @@
+[CmdletBinding()]
+Param(
+[Parameter(Mandatory = $True)]
+[String]
+$source_dir,
+
+[Parameter(Mandatory = $True)]
+[String]
+$target_dir
+)
+
+# Create date stamp for backup sub directory
+$date_stamp = Get-Date -UFormat "+%Y%m%d-%H%M%S"
+
+# Create subdir with timestamp in target backup dir
+$full_target_backup_path = Join-Path -Path $target_dir -ChildPath "site_backup_$date_stamp"
+
+# Copy contents of source dir to full backup path target
+Write-Output "Copying items from $source_dir to full backup path $full_target_backup_path"
+Copy-Item -Recurse -Path $source_dir -Destination $full_target_backup_path
